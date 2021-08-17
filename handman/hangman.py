0500a13f4a636a56f21957d7ea3043ba47f764c4
Alexander Adedeji <alexadedeji15@gmail.com>
2021-08-17 14:16:32 +0100

    added lives to hang man

--- handman/hangman.py
+++ handman/hangman.py
@@ -15,14 +15,17 @@ def generate_valid_word(words):
 
 
 def hangman():
+    live = 6
     word = generate_valid_word(words).upper()
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and live > 0:
+        print(f' You have {live} lives left ')
+        print('')
         print('You have used these letters: ', ' '.join(used_letters))
-
+        print('')
         word_list = [letter if letter in used_letters else '_' for letter in word]
         # word_list = []
         # for letter in word:
@@ -39,13 +42,19 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                live = live -1
+                print('letter not in word')
         elif user_letter in used_letters:
             print('You have already used that character. Please try again.')
             
         else:
             print('Invalid character. Please try again. ')
     
-    print('Hey, Congratulations, You Won!!')
+    if live == 0:
+        print(f'You died, sorry, The word was {word}')
+    else:
+        print('Hey, Congratulations, You Won!!')
             
 
     
